0fdd9b1af736b31187fe320ce4b6d92c40edd4d8
Ekaterina <88279403+EkaterinaT89@users.noreply.github.com>
2021-09-10 15:46:52 +0300

    Update main.kt
    Перенос точки

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,7 +3,7 @@ fun main() {
     val amount: Int = 7_000
 
     if (amount >= 3_500) {
-        val commission = amount / 100 * .075
+        val commission = amount / 100 * 0.75
         println("Размер коммисии $commission")
     } else println("Слишком маленькая сумма!")
 
